49fa9d6da795e746a050f7411064b6a600ab5426
Eyder Santiago Suarez Chavez <eyder.suarez@correounivalle.edu.co>
2024-10-22 22:06:45 -0500

    cambios eyder

---
+++ servicio_santi/santi-service.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/greeting', (req, res) => {
+  res.json({ mensaje: "👋 Hi, I'm Santiago Sánchez, 🔭 I’m currently working with 🐍" });
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Santiago Service listening on http://localhost:${port}`);
+});
