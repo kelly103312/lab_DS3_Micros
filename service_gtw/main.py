a61d578995c95324dd6b638a8fb36a554fd4ab33
kelly103312 <desarrollador1aj@gmail.com>
2024-10-22 19:44:24 -0500

    Gtw

---
+++ service_gtw/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+
+@app.get("/orquestar")
+async def orquestar():
+    async with httpx.AsyncClient() as client:
+        try:
+            respuesta_a = await client.get("http://servicio-kelly-service/info")
+            data_a = respuesta_a.json()
+        except httpx.RequestError:
+            data_a = "El servicio A no está disponible"
+
+
+    return {"respuesta_a": data_a}
