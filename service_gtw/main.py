71ffb2ae11cf3edb38e6e11ee110fecb604c6af1
LeninCar <lenin.carabali@correounivalle.edu.co>
2024-10-22 20:04:30 -0500

    lenin micro fixed2

--- service_gtw/main.py
+++ service_gtw/main.py
@@ -11,6 +11,13 @@ async def orquestar():
             data_a = respuesta_a.json()
         except httpx.RequestError:
             data_a = "El servicio A no está disponible"
+    
+    async with httpx.AsyncClient() as client:
+        try:
+            respuesta_a = await client.get("http://serviciolenin:8000/serviciolenin")
+            data_a = respuesta_a.json()
+        except httpx.RequestError:
+            data_a = "El servicio de Lenin no está disponible"
 
 
     return {"respuesta_a": data_a}
